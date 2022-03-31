acf2c370b14ec98689669dcb87f5d5997d1c60a9
LongTranIT <tranphilongnvd@gmail.com>
2022-03-31 16:09:44 +0700

    complete search

--- src/app/controllers/AnimalController.js
+++ src/app/controllers/AnimalController.js
@@ -1,46 +1,61 @@
+const dotenv = require("dotenv/config");
+const Animal = require("../models/Animal");
 
-const dotenv = require('dotenv/config');
-const Animal =require('../models/Animal');
-
-class AnimalController{
+class AnimalController {
     //[GET] /
-    showHome(req,res){
-        res.render('home');
+    showHome(req, res) {
+        res.render("home");
     }
-    showDiscover(req,res){
-        if(req.query.hasOwnProperty('q')){
-            const search=req.query.q;
-            Animal.find({vietnamse_name:{$regex: req.query.q, $options: 'i'}})
+    showDiscover(req, res) {
+        let query={};
+        let search="";
+        if (req.query.hasOwnProperty("q")) {
+            query.vietnamse_name= { $regex: req.query.q, $options: "i" };
+            search = req.query.q;
+        } 
+        if (req.query.hasOwnProperty("phylum")) {
+            query.phylum= req.query.phylum;
+            search += req.query.phylum+"-";
+        } 
+        if (req.query.hasOwnProperty("class")) {
+            query.class= req.query.class;
+            search += req.query.class+"-";
+        } 
+        if (req.query.hasOwnProperty("order")) {
+            query.order= req.query.order;
+            search += req.query.order+"-";
+        } 
+        if (req.query.hasOwnProperty("family")) {
+            query.family= req.query.family;
+            search += req.query.family+"-";
+        } 
+        if (req.query.hasOwnProperty("conservation_status_red_book_vn")) {
+            query.conservation_status_red_book_vn= req.query.conservation_status_red_book_vn;
+            search += req.query.conservation_status_red_book_vn+"-";
+        } 
+        Animal.find(query)
+                .lean()
+                .limit(6)
+                .then((data) => res.render("discover", { data, search }))
+                .catch((err) => res.json(err));
+    }
+
+    showInfo(req, res) {
+        Animal.findOne({ slug: req.params.slug })
             .lean()
-            .limit(6)
-            .then(data=>res.render('discover',{data,search}))
-            .catch(err=>res.json(err))
-        }
-        else{
-            Animal.find({})
-            .lean()
-            .limit(6)
-            .then(data=>res.render('discover',{data}))
-            .catch(err=>res.json(err))
-        }
+            .then((animal) => res.render("info", { animal }))
+            .catch((err) => res.json(err));
     }
-        
-    showInfo(req,res){
-        Animal.findOne({slug:req.params.slug})
-            .lean()
-            .then(animal=>res.render('info',{animal}))
-            .catch(err=>res.json(err))
+    search(req, res) {
+        res.render("search");
     }
-    search(req,res){
-        res.render('search');
+    aboutUs(req, res) {
+        res.render("aboutUS");
     }
-    aboutUs(req,res){
-        res.render('aboutUS');
-    }
-    map(req,res){
-        const key=process.env.MAP_API_KEY+"";
-        res.render('map',{key});
+    map(req, res) {
+        const key = process.env.MAP_API_KEY + "";
+        res.render("map", { key });
     }
 }
 
-module.exports= new AnimalController;
+module.exports = new AnimalController();
